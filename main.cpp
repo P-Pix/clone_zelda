290a6a171d67aa1b4c448367bd21b0c8dbb9e935
P-Pix <guillaume200202@gmail.com>
2021-03-07 00:07:43 +0100

    code 06/2021

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ sf::Texture open_texture(const char *NAME_IMAGE)
     sf:: Texture TEXTURE;
     if(!TEXTURE.loadFromFile(NAME_IMAGE))
     {
-        std::cout << "error" << std::endl;
+        std::cout << "error image " << NAME_IMAGE << std::endl;
     }
     return TEXTURE;
 }
@@ -16,35 +16,52 @@ sf::Vector2f move_direct()
 {
     if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
     {
-        return sf::Vector2f(-1.f, 0.f);
+        return sf::Vector2f(-2.f, 0.f);
     }
     else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
     {
-        return sf::Vector2f(1.f, 0.f);
+        return sf::Vector2f(2.f, 0.f);
     }
     else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
     {
-        return sf::Vector2f(0.f, -1.f);
+        return sf::Vector2f(0.f, -2.f);
     }
     else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
     {
-        return sf::Vector2f(0.f, 1.f);
+        return sf::Vector2f(0.f, 2.f);
     }
     return sf::Vector2f(0.f, 0.f);
+}
+
+sf::Font open_font(const char *NAME_FONT)
+{
+    sf::Font font;
+    if(!font.loadFromFile(NAME_FONT))
+    {
+        std::cout << "error font " << NAME_FONT << std::endl;
+    }
+    return font;
 }
 
 int main()
 {
     const char *NAME_IMAGE = "skin/heros1.png";
+    const char *NAME_FONT_2 = "font/SuperLegendBoy.ttf";
 
     sf::Texture IMAGE_1;
     sf::Sprite SPRITE;
+    sf::Font FONT_LEMON, FONT_8BIT;
+    sf::Text TEXTE("Hello SFML", FONT_8BIT, 150); 
+    sf::RenderWindow WINDOW(sf::VideoMode(1080, 720), "Zelda_Like");
     //sf::Vector2f VECTOR = (1.f, 1.f);
 
     IMAGE_1 = open_texture(NAME_IMAGE);
     SPRITE.setTexture(IMAGE_1);
 
-    sf::RenderWindow WINDOW(sf::VideoMode(1080, 720), "Zelda_Like");
+    FONT_8BIT = open_font(NAME_FONT_2);
+
+    WINDOW.setFramerateLimit(60);
+
     while(WINDOW.isOpen())
     {
         sf::Event event;
@@ -58,6 +75,7 @@ int main()
         SPRITE.move(move_direct());
         WINDOW.clear();
         WINDOW.draw(SPRITE);
+        //WINDOW.draw(TEXTE);
         WINDOW.display();
     }
 }
